6809b61e4a72424fb6042048edfff6b3e1f28d30
kronos0409 <jaranicolas101@gmail.com>
2024-11-28 17:35:48 -0300

    Add files via upload

--- calcu.py
+++ calcu.py
@@ -4,7 +4,9 @@ notas=[]
 ponderaciones=[]
 
 st.title("Bienvenido a tu sufrimiento")
-
+seguir_nota = True
+seguir_ponderacion = True
+comprobar = 0
 cant_notas = st.number_input("Cuantas notas tienes?", min_value=1, max_value=10)
 eximicion = st.text_input("Con que nota te eximes?")
 c1,c2,c3 = st.columns(3)
@@ -25,23 +27,37 @@ try:
             promedio+=notas[i]*ponderaciones[i]
     with c1: 
         ponderacion = st.text_input("Introduce el porcentaje de tu nota que necesitas", key="B")
+        for i in range(0, len(notas)):
+            if float(notas[i])<=0:
+                seguir_nota = False
+            if float(ponderaciones[i])<=0:
+                seguir_ponderacion = False
+            comprobar +=float(ponderaciones[i])
+        if comprobar + (float(ponderacion)/100) != 1:
+            seguir_ponderacion= False 
     with c3:
-        nota_necesaria = float((-promedio+float(eximicion)))/(float(ponderacion)/100)
-        if nota_necesaria<0:
-            st.write("ya pasaste")
-        if float(nota_necesaria)>=6.0:
-            st.image("8634ecb456af4ded64541504fb9a67c7.jpg")
-            st.write(f"KGASTE, necesitas un {np.round(nota_necesaria,decimals=1)}")
-            st.write(f"Con un {np.round(nota_necesaria,decimals=1)} al {ponderacion}% te sumaria {np.round(nota_necesaria*(float(ponderacion)/100),decimals=1)}")
-        elif float(nota_necesaria)>=2.0:
-            st.image("30100440.jpg")
-            st.write(f"Tu puedess, necesitas un {np.round(nota_necesaria,decimals=1)}")
-            st.write(f"Con un {np.round(nota_necesaria,decimals=1)} al {ponderacion}% te sumaria {np.round(nota_necesaria*(float(ponderacion)/100),decimals=1)}")
+        if seguir_nota and seguir_ponderacion:
+            nota_necesaria = float((-promedio+float(eximicion)))/(float(ponderacion)/100)
+            if nota_necesaria<0:
+                st.write("ya pasaste")
+            if float(nota_necesaria)>=6.0:
+                st.image("8634ecb456af4ded64541504fb9a67c7.jpg")
+                st.write(f"KGASTE, necesitas un {np.round(nota_necesaria,decimals=1)}")
+                st.write(f"Con un {np.round(nota_necesaria,decimals=1)} al {ponderacion}% te sumaria {np.round(nota_necesaria*(float(ponderacion)/100),decimals=2)}")
+            elif float(nota_necesaria)>=2.0:
+                st.image("30100440.jpg")
+                st.write(f"Tu puedess, necesitas un {np.round(nota_necesaria,decimals=1)}")
+                st.write(f"Con un {np.round(nota_necesaria,decimals=1)} al {ponderacion}% te sumaria {np.round(nota_necesaria*(float(ponderacion)/100),decimals=2)}")
+            else:
+                st.image("1522304.jpg")
+                st.write(f"necesitas un {np.round(nota_necesaria,decimals=1)}")
+                st.write(f"Con un {np.round(nota_necesaria,decimals=1)} al {ponderacion}% te sumaria {np.round(nota_necesaria*(float(ponderacion)/100),decimals=2)}")
+            st.write(f"Tu promedio actual es:    {np.round(promedio,decimals=2)}")
         else:
-            st.image("1522304.jpg")
-            st.write(f"necesitas un {np.round(nota_necesaria,decimals=1)}")
-            st.write(f"Con un {np.round(nota_necesaria,decimals=1)} al {ponderacion}% te sumaria {np.round(nota_necesaria*(float(ponderacion)/100),decimals=1)}")
-        st.write(f"Tu promedio actual es:    {np.round(promedio,decimals=1)}")
+            if seguir_nota== False:
+                st.subheader("Las notas deben ser valores positivos")
+            if seguir_ponderacion == False:
+                st.subheader("Las ponderaciones deben ser valores positivos y deben de sumar 100%")
     
 except ValueError:
     st.write("complete los datos")
